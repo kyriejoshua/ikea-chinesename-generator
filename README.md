e21964b1cab30e8f7f1e1ccec65691b3c1625bdd
kyriejoshua <withoutIverson@outlook.com>
2020-09-14 21:05:05 +0800

    :boom: first commit

---
+++ README.md
@@ -0,0 +1,37 @@
+
+## Installation
+
+```shell
+npm install ikea-chinesename-generator --save
+yarn add ikea-chinesename-generator
+```
+
+## Usage
+
+* Javascript
+```javascript
+import generateIkeaLikeName from 'ikea-chinesename-generator';
+
+const ikeaName = generateIkeaLikeName();
+console.info(ikeaName);
+```
+
+## Demo
+
+```shell
+"德贝帝斯"
+"斯库勒"
+"麦穆拉维玛"
+"麦库维勒克"
+"贝夫格维拉"
+"玛托利"
+"库斯"
+"夫穆德夫斯"
+"夫卡利特帝"
+"库拉利玛拉"
+"格勒帝利勒"
+```
+
+### That's all
+
+* hahaha.
---
+++ index.js
@@ -0,0 +1,43 @@
+/*
+* @Author: kyriejoshua
+* @Date:   2020-09-14 20:49:26
+* @Last Modified by:   changri
+* @Last Modified time: 2020-09-14 20:58:59
+*/
+
+/**
+ * 关键字
+ */
+export const IKEA_NAMES_KEYWORDS = ['拉', '斯', '夫', '帝', '格', '托', '玛', '穆', '勒', '库', '卡', '利', '麦', '维', '克', '特', '贝', '德'];
+
+/**
+ * 获取随机数
+ * @param zoom
+ */
+export const generateRandomNumber = (zoom = 100) => {
+  return Math.random() * zoom;
+}
+
+/**
+ * 基于基准值生成长度
+ * @param base
+ */
+export const generateLength = (base = 4) => {
+  return window.parseInt(generateRandomNumber() % base);
+}
+
+/**
+ * 随机生成一个宜家风格的中文名字
+ */
+const generateIkeaLikeName = () => {
+  const nameLength = generateLength() + 1;
+  let ikeaName = '';
+
+  for (let i = 0; i <= nameLength; i++) {
+    const index = generateLength(IKEA_NAMES_KEYWORDS.length);
+    ikeaName += IKEA_NAMES_KEYWORDS[index];
+  }
+  return ikeaName;
+}
+
+export default generateIkeaLikeName;

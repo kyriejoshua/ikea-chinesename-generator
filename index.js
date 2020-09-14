119be54c564f7e8b451b3bcce4e87d533bd03e9f
kyriejoshua <withoutIverson@outlook.com>
2020-09-14 21:37:05 +0800

    :bulb: 减值迭代

--- index.js
+++ index.js
@@ -21,12 +21,13 @@ export const generateLength = (base = 4) => {
 
 /**
  * 随机生成一个宜家风格的中文名字
+ * 加 1 位避免生成一位的名字
  */
 const generateIkeaLikeName = () => {
   const nameLength = generateLength() + 1;
   let ikeaName = '';
 
-  for (let i = 0; i <= nameLength; i++) {
+  for (let i = nameLength; i >= 0; i--) {
     const index = generateLength(IKEA_NAMES_KEYWORDS.length);
     ikeaName += IKEA_NAMES_KEYWORDS[index];
   }
